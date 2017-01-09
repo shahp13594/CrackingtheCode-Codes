552ad3775185900a8cd90242d08730b5dc53498c
Parth Shah <shahp13594@gmail.com>
2017-01-09 01:30:16 -0800

    Update PalindromePermutation.java

--- Arrays and Strings/PalindromePermutation.java
+++ Arrays and Strings/PalindromePermutation.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 
 public class PalindromePermutation {
@@ -8,12 +10,22 @@ public class PalindromePermutation {
 		// TODO Auto-generated method stub
        Scanner sc=new Scanner(System.in);
        String s=sc.nextLine();
-       s=s.toLowerCase();
-       s=s.replaceAll("\\s","");
-       char ch[]=s.toCharArray();
-       Arrays.sort(ch);
-       
-       System.out.println(ch);
+      Set<Character> set=new HashSet<Character>();
+       for(int i=0; i<s.length(); ++i){
+           if (!set.contains(s.charAt(i)))
+               set.add(s.charAt(i));
+           else 
+               set.remove(s.charAt(i));
+       }
+       if(set.size()==0 || set.size()==1)
+       {
+    	   System.out.println("It is permutation of palindromw");
+       }
+       else
+       {
+    	   System.out.println("It is not a permutation of palindromw");
+
+       }
 	}
 
 }
